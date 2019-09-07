c13d436aea97c99f9e88f1dc9f5ba6f5a4eba362
Kevin <me@bradtravery.com>
2019-09-07 14:46:58 -0400

    add sign up

--- frontend/index.js
+++ frontend/index.js
@@ -8,6 +8,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
 
     document.getElementById("user_div").style.display = "block";
+    document.getElementById("signUp_div").style.display = "none";
     document.getElementById("login_div").style.display = "none";
 
     var user = firebase.auth().currentUser;
@@ -23,16 +24,43 @@ firebase.auth().onAuthStateChanged(function(user) {
     // No user is signed in.
 
     document.getElementById("user_div").style.display = "none";
+    document.getElementById("signUp_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
 
   }
 });
+
+function SwitchToLogin() {
+  document.getElementById("login_div").style.display = "block";
+  document.getElementById("signUp_div").style.display = "none";
+}
+
+function SwitchToSignUp() {
+  document.getElementById("login_div").style.display = "none";
+  document.getElementById("signUp_div").style.display = "block";
+}
 
 function login(){
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function signUp(){
+  var userEmail = document.getElementById("email_field_sign").value;
+  var userPass = document.getElementById("password_field_sign").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
